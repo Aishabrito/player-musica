6ee55ee11c97f2255802ab0b41cf68c0be4586bd
AishaBrito <aisha.paola.20221@poli.ufrj.br>
2025-12-17 18:01:38 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-blue-600 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+        Tailwind OK
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",        // arquivo HTML principal na raiz
+    "./src/**/*.{js,jsx}"  // todos os componentes React dentro de src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
