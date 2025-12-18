c636f92e6a4e4567ea5df16bfbc763148a31b513
AishaBrito <aisha.paola.20221@poli.ufrj.br>
2025-12-18 16:24:42 -0300

    fundo tela player responsiva

--- src/pages/PlayerPage.jsx
+++ src/pages/PlayerPage.jsx
@@ -1,10 +1,19 @@
 import Player from "../components/Player";
+import fundo from "../assets/fundoplayer.png";
 
 export default function PlayerPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-start bg-gray-200">
-      <h1 className="text-4xl font-bold mt-10 mb-6">🎶 Notas Ocultas</h1>
-      <Player />
+    <div
+      className="min-h-screen w-full bg-cover bg-center bg-no-repeat bg-fixed"
+      style={{ backgroundImage: `url(${fundo})` }}
+    >
+      <div className="min-h-screen w-full flex flex-col items-center justify-start px-4 backdrop-brightness-75">
+        <h1 className="text-4xl font-bold mt-10 mb-6 text-white drop-shadow-lg font-sans">
+          🎶 Notas Ocultas
+        </h1>
+
+        <Player />
+      </div>
     </div>
   );
 }
